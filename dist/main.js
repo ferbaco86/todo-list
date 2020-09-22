e3842458d07af98bc938623e9fd8ed8cf34ec498
Fernando Bahamondes <ferbaco@gmail.com>
2020-09-22 14:46:10 -0300

    Add links to css and js in index.html

--- dist/main.js
+++ dist/main.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("console.log('Test');\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
