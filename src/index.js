8bbe0cf0eb07784ac0e26d0da4b1cbcc3e249b77
Fernando Bahamondes <ferbaco@gmail.com>
2020-09-23 14:45:52 -0300

    Add project factory and add project button functionality

--- src/index.js
+++ src/index.js
@@ -1,2 +1,46 @@
 import domManipulation from './helpers';
 import './style.css';
+
+let counter = 0;
+const project = (title, id) => {
+  const getTitle = () => title;
+  const getId = () => id;
+
+  const setTitle = (newTitle) => {
+    title = newTitle;
+  };
+
+  const setId = (newId) => {
+    id = newId;
+  };
+
+  return {
+    getTitle,
+    getId,
+    setTitle,
+    setId,
+  };
+};
+
+const newProject = project('', '');
+
+const btnAddProject = document.getElementById('btn-add-project');
+btnAddProject.addEventListener('click', () => {
+  const projectContainer = document.getElementById('project-container');
+  const projectTitleInput = document.getElementById('project-title').value;
+  newProject.setTitle(projectTitleInput);
+  newProject.setId(counter);
+
+  const projectTitle = newProject.getTitle();
+
+  console.log(newProject.getId());
+  console.log(newProject.getId());
+
+
+  const btnProject = domManipulation.createHtmlElement({
+    tag: 'button', parentElement: projectContainer, arrayClassNames: ['button', 'project-btn'], text: projectTitle,
+  });
+
+  btnProject.setAttribute('id', counter);
+  counter += 1;
+});
